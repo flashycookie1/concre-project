b7d4d8b372772ab60b13006f899b24f18a86b5f6
GeorgeJ <flashycookie1@gmail.com>
2024-06-26 22:59:44 -0400

    "added groupme icon"

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,8 @@ export const Navbar = () => {
   
   const uconntact = "https://uconntact.uconn.edu/organization/concretecanoeassociationuconn";
   const instagram = "https://www.instagram.com/uconnconcretecanoe?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
-  
+  const groupme = "https://groupme.com/join_group/55277612/5LKpXhFn"
+
   return (
     <>
       <img className="logo" src="src/assets/logoDark.svg" alt="UCONN Concrete Canoe Logo Here"width="13%" height="auto"></img>
@@ -15,7 +16,8 @@ export const Navbar = () => {
     </ul>
     </nav>
     <div>
-      <a className="icon" href="#" target="_blank"><img src="src/assets/mail.svg" width="20px" height="auto"></img></a>
+      <a className="icon" title="Contact Us"  href="#" target="_blank"><img src="src/assets/mail.svg" width="20px" height="auto"></img></a>
+      <a className="icon" href={groupme} target="_blank"><img src="src/assets/hash.svg" width="20px" height="auto"></img></a>
       <a className="icon" href={instagram} target="_blank"><img src="src/assets/instagram.svg" width="20px" height="auto"></img></a>
       <a className="cta" href={uconntact} target="_blank"><button>Join UConntact</button></a></div>
     </>
