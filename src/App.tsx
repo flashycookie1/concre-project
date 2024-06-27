408ad3a49421f661d2309a6f5660979b6dc49794
GeorgeJ <flashycookie1@gmail.com>
2024-06-26 22:15:56 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import { Navbar } from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+      </header>
+    </>
+  );
+}
+
+export default App;
