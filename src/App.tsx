fda2a8b9a0c994b37de592a8d51dd12046f356e0
Mia Larkin <mia.larkin1@icloud.com>
2024-07-02 17:54:36 -0400

    newsboard

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,18 @@ function App() {
 
       {/* NewsBoard Set up as a grid */}
       <div className="newsBoard">
-
+        <div>
+          <h1 className="title">
+            What We're Up To
+          </h1>
+        </div>
+        <div>
+          <ul className="list-group">
+            <li className="list-group-item">UConn takes 4th place overall at the ASCE Northeastern Student Symposium</li>
+            <li className="list-group-item">We're currently looking for Sponsors</li>
+            <li className="list-group-item">Planning for the 2024-2025 Northeastern Competition is underway</li>
+          </ul>
+        </div>
       </div>
       
     </>
