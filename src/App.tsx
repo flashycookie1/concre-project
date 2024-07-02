5fbbbc12424cd606f0c2c920faea4d12fe3d410c
Mia Larkin <mia.larkin1@icloud.com>
2024-07-02 17:11:02 -0400

    Set into div

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,22 @@ import { Navbar } from "./components/navbar";
 function App() {
   return (
     <>
-      <header>
-        <Navbar></Navbar>
-      </header>
+      <div>
+        {/* Set the header to be the NavBar */}
+        <header>
+          <Navbar></Navbar>
+        </header>
+      </div>
+
+      {/* Insert image as background */}
+      <img className="background" src="src/assets/bgwtitle.svg"></img>
+
+
+      {/* NewsBoard Set up as a grid */}
+      <div className="newsBoard">
+
+      </div>
+      
     </>
   );
 }
