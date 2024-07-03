437cfc4ec566cd81746488a62d60c916e2a23e03
Mia Larkin <mia.larkin1@icloud.com>
2024-07-03 13:05:45 -0400

    WIP image slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import { Navbar } from "./components/navbar";
+import { MiniGallery } from "./components/minigallery";
 import { NewsBoard } from "./components/newsboard";
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
         <header>
           <Navbar></Navbar>
         </header>
-        {/* Insert image as background */}
+        {/* Insert image as Backdrop */}
         <img className="background" src="src/assets/bgwtitle.svg"></img>
+        {/* MiniGallery Component*/}
+        <MiniGallery></MiniGallery>
         {/* NewsBoard Component */}
         <NewsBoard></NewsBoard>
       </div>
