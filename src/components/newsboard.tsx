789484ec12c025168865c0d762a4b56720018505
Mia Larkin <mia.larkin1@icloud.com>
2024-07-03 11:56:09 -0400

    buttons on newsboard

--- src/components/newsboard.tsx
+++ src/components/newsboard.tsx
@@ -9,11 +9,32 @@ export const NewsBoard = () => {
         </div>
         <div>
           <ul className="list-group">
-            <li className="list-group-item">UConn takes 4th place overall at the ASCE Northeastern Student Symposium</li>
-            <li className="line">______________________________________________________________</li>
-            <li className="list-group-item">We're currently looking for Sponsors</li>
-            <li className="line">______________________________________________________________</li>
-            <li className="list-group-item">Planning for the 2024-2025 Northeastern Competition is underway</li>
+            <div className="text-button-grid">
+              <div>
+                <li className="list-group-item">UConn takes 4th place overall at the ASCE Northeastern Student Symposium</li>
+              </div>
+              <div>
+                <button className="b1"> Visit Gallery </button>
+              </div>  
+            </div>
+            <li className="line">________________________________________________________________________________</li>
+            <div className="text-button-grid">
+              <div>
+                <li className="list-group-item">We're currently looking for Sponsors</li>
+              </div>
+              <div>
+                <button className="b1"> Sponsor Us </button>
+              </div>  
+            </div>
+            <li className="line">________________________________________________________________________________</li>
+            <div className="text-button-grid">
+              <div>
+                <li className="list-group-item">Planning for the 2024-2025 Northeastern Competition is underway</li>
+              </div>
+              <div>
+                <button className="b1"> View Process </button>
+              </div>  
+            </div>
           </ul>
         </div>
     </div>
