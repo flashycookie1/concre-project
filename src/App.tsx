8bc1dd6a4238e5805374aa42df852349246543d4
George Ji <flashycookie1@gmail.com>
2024-07-03 13:52:55 -0400

    reverted to source version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 //import { useState } from 'react'
-<<<<<<< HEAD
-import Navbar from "./components/navbar";
-import NewsBoard from "./components/newsboard";
-import Hero from "./components/hero";
-=======
 import { Navbar } from "./components/navbar";
 import { MiniGallery } from "./components/minigallery";
 import { NewsBoard } from "./components/newsboard";
->>>>>>> 9088b40e3364d79fc712e3604a3e155c29f0d0aa
 
 function App() {
   return (
@@ -15,15 +9,11 @@ function App() {
         <header>
           <Navbar></Navbar>
         </header>
-<<<<<<< HEAD
-        <Hero></Hero>
-=======
         {/* Insert image as Backdrop */}
         <img className="background" src="src/assets/bgwtitle.svg"></img>
         {/* MiniGallery Component*/}
         <MiniGallery></MiniGallery>
         {/* NewsBoard Component */}
->>>>>>> 9088b40e3364d79fc712e3604a3e155c29f0d0aa
         <NewsBoard></NewsBoard>
     </>
   );
