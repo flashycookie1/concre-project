3eef8304148699763aefc535c2a4842617d7ede7
George Ji <flashycookie1@gmail.com>
2024-07-02 21:20:38 -0400

    added fonts + turned newsboard into a component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import { Navbar } from "./components/navbar";
+import { NewsBoard } from "./components/newsboard";
 
 function App() {
   return (
@@ -9,30 +10,10 @@ function App() {
         <header>
           <Navbar></Navbar>
         </header>
+        {/* Insert image as background */}
+        <img className="background" src="src/assets/bgwtitle.svg"></img>
+        <NewsBoard></NewsBoard>
       </div>
-
-      {/* Insert image as background */}
-      <img className="background" src="src/assets/bgwtitle.svg"></img>
-
-
-      {/* NewsBoard Set up as a grid */}
-      <div className="newsBoard">
-        <div>
-          <h1 className="title">
-            What We're Up To
-          </h1>
-        </div>
-        <div>
-          <ul className="list-group">
-            <li className="list-group-item">UConn takes 4th place overall at the ASCE Northeastern Student Symposium</li>
-            <li className="line">______________________________________________________________</li>
-            <li className="list-group-item">We're currently looking for Sponsors</li>
-            <li className="line">______________________________________________________________</li>
-            <li className="list-group-item">Planning for the 2024-2025 Northeastern Competition is underway</li>
-          </ul>
-        </div>
-      </div>
-      
     </>
   );
 }
