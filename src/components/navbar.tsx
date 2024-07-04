ae82c31f4669681258841952e26e0bd6ae0d352e
George Ji <flashycookie1@gmail.com>
2024-07-04 18:20:59 -0400

    temporarily disabled background and centered navbar by adjusting the logo's right margin

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,10 +3,11 @@ const Navbar = () => {
   const uconntact = "https://uconntact.uconn.edu/organization/concretecanoeassociationuconn";
   const instagram = "https://www.instagram.com/uconnconcretecanoe?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
   const groupme = "https://groupme.com/join_group/55277612/5LKpXhFn"
+  const email = "";
 
   return (
     <>
-      <img className="logo" src="src/assets/logo.png" alt="UCONN Concrete Canoe Logo Here"width="10%" height="auto"></img>
+      <header><img className="logo" src="src/assets/logo.png" alt="UCONN Concrete Canoe"width="7%" height="auto"></img>
         <nav>
           <ul>
             <li className="navlink"><a className="selected" href="#">Home</a></li>
@@ -15,11 +16,11 @@ const Navbar = () => {
           </ul>
         </nav>
       <div>
-        <a className="icon" title="Contact Us" href="#" target="_blank"><img src="src/assets/mail.svg" width="20px" height="auto"></img></a>
-        <a className="icon" href={groupme} target="_blank"><img src="src/assets/hash.svg" width="20px" height="auto"></img></a>
-        <a className="icon" href={instagram} target="_blank"><img src="src/assets/instagram.svg" width="20px" height="auto"></img></a>
+        <a className="icon" href={groupme} target="_blank"><img src="src/assets/hash.svg" width="18px" height="auto"></img></a>
+        <a className="icon" href={instagram} target="_blank"><img src="src/assets/instagram.svg" width="18px" height="auto"></img></a>
         <a className="cta" href={uconntact} target="_blank"><button>Join UConntact</button></a>
       </div>
+      </header>
     </>
   );
 };
