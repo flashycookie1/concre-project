025e56bd72e5e178d4e15ecad39c0f141f255ea0
George Ji <flashycookie1@gmail.com>
2024-07-04 19:36:36 -0400

    refactored code

--- src/components/newsboard.tsx
+++ src/components/newsboard.tsx
@@ -1,41 +1,45 @@
 const NewsBoard = () => {
   return (
-    <div className="newsBoardFlex">
       <div className="newsBoard">
-          <h1 className="title">
-            WHAT WE'RE UP TO
-          </h1>
-          <ul className="list-group">
-            <div className="text-button-grid">
-              <div>
-                <li className="list-group-item1">UConn takes 4th place overall at the ASCE Northeastern Student Symposium</li>
-              </div>
-              <div className="b1Div">
-                <button className="b1"> Visit Gallery </button>
-              </div>  
+        <h1 className="title">WHAT WE'RE UP TO</h1>
+        <ul>
+          <div className="text-button-grid">
+            <li className="list-group-item">
+              UConn takes 4th place overall at the ASCE<br/>
+              Northeastern Student Symposium
+            </li>
+            <div className="b1Div">
+              <button className="b1"> Visit Gallery </button>
             </div>
-            <li className="line">___________________________________________________________________________________________</li>
-            <div className="text-button-grid">
-              <div>
-                <li className="list-group-item2">We're currently looking for Sponsors</li>
-              </div>
-              <div className="b1Div">
-                <button className="b1"> Sponsor Us </button>
-              </div>  
+          </div>
+          <hr />
+          <div className="text-button-grid">
+            <div>
+              <li className="list-group-item">
+                We're currently looking<br/>
+                for Sponsors
+              </li>
             </div>
-            <li className="line">___________________________________________________________________________________________</li>
-            <div className="text-button-grid">
-              <div>
-                <li className="list-group-item3">Planning for the 2024-2025 Season is underway!</li>
-              </div>
-              <div className="b1Div">
-                <button className="b1"> View Process </button>
-              </div>  
+            <div className="b1Div">
+              <button className="b1"> Sponsor Us </button>
             </div>
-          </ul>
-        </div>
-    </div>
-  )
-}
+          </div>
+          <hr/>
+          <div className="text-button-grid">
+            <div>
+              <li className="list-group-item">
+                Planning for the 2024-2025<br/>
+                Season is underway!
+              </li>
+            </div>
+            <div className="b1Div">
+              <button className="b1"> View Process </button>
+            </div>
+          </div>
+          <hr />
+        </ul>
+      </div>
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
