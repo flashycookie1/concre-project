4ccd56bfeb9ecccf172a102b013ed28f68a184c7
George Ji <flashycookie1@gmail.com>
2024-07-05 14:43:04 -0400

    added text

--- src/components/challenge.tsx
+++ src/components/challenge.tsx
@@ -1,7 +1,17 @@
 const Challenge = () => {
   return (
-    <div className="challengeDiv"><h1 className="challengeHead">The Challenge</h1></div>
-  )
-}
+    <div className="challengeDiv">
+      <h1 className="challengeHead">The Challenge</h1>
+      <p className="challengetxt">
+        Each year, it is the team’s challenge to assess and evaluate competition
+        regulations and adhere to them throughout the engineering design
+        process. This includes designing, planning, testing, constructing, and
+        finalizing. At the end of the process, the team kicks into competition
+        mode where we deliver an in-depth design paper, give a panel
+        presentation, and race and compete against other teams in our canoe.
+      </p>
+    </div>
+  );
+};
 
-export default Challenge
+export default Challenge;
