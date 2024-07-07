5607e92fe2ca9a0f4aeed4a6cd4e02ad08991dc5
Mia Larkin <mia.larkin1@icloud.com>
2024-07-07 15:28:19 -0400

    footer navbar wip

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Hero from "./components/hero";
 import NewsBoard from "./components/newsboard";
 import Challenge from "./components/challenge";
 import Sponsor from "./components/sponsor";
+import NavBarFooter from "./components/navbarFooter";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <NewsBoard></NewsBoard>
         <Challenge></Challenge>
         <Sponsor></Sponsor>
+        <NavBarFooter></NavBarFooter>
     </>
   );
 }
