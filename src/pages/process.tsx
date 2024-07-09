825da2a4e68dc529cf42d8e08108b03a3c7991c5
Mia Larkin <mia.larkin1@icloud.com>
2024-07-09 18:16:03 -0400

    wip timeline for process

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -1,10 +1,112 @@
 import Navbar from "../components/navbar";
 import NavBarFooter from "../components/navbarFooter";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import '../process.css'
 
 const Process = () =>{
     return(
         <>
             <Navbar></Navbar>
+            <div className="small-transition">
+                <p className="small-trans-text" style={{paddingBottom:'50px'}}>
+                    How we go from 
+                </p>
+                <div className="small-transitionDiv">
+                    <img src="src/assets/start.jpg" alt="start" style={{width:'400px'}}/>
+                    <p className="small-trans-text" style={{width:'300px', textAlign:'center'}}>
+                        to
+                    </p>
+                    <img src="src/assets/end.jpeg" alt="end" style={{width:'400px'}}/>
+                </div>
+            </div>
+            <div className="timeline">
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2011 - present"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                        <p>
+                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2010 - 2011"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Art Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2008 - 2010"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                        <p>
+                        User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2006 - 2008"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                        User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="April 2013"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                        <p>
+                        Strategy, Social Media
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="November 2012"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+                        <p>
+                        Creative Direction, User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="2002 - 2006"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                        <p>
+                        Creative Direction, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    />
+                </VerticalTimeline>
+            </div>
+            
             <NavBarFooter></NavBarFooter>
         </>
     )
