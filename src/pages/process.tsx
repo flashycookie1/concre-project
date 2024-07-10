a427f7ee6a90573306943a26ab2dfd535c00fb70
Mia Larkin <mia.larkin1@icloud.com>
2024-07-09 20:48:51 -0400

    process/timeline complete

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -12,13 +12,13 @@ const Process = () =>{
                     How we go from 
                 </p>
                 <div className="small-transitionDiv">
-                    <img src="src/assets/start.jpg" alt="start" style={{width:'400px'}}/>
-                    <p className="small-trans-text" style={{width:'300px', textAlign:'center'}}>
+                    <img src="src/assets/timeline/start.jpg" alt="start" style={{width:'380px'}}/>
+                    <p className="small-trans-text" style={{width:'340px', textAlign:'center'}}>
                         to
                     </p>
-                    <img src="src/assets/end.jpeg" alt="end" style={{width:'400px'}}/>
+                    <img src="src/assets/timeline/end.jpeg" alt="end" style={{width:'380px'}}/>
                 </div>
-                <p className="small-trans-text" style={{paddingTop:'50px'}}>
+                <p className="small-trans-text" style={{paddingTop:'50px', textAlign:'center'}}>
                     Check out the Process Below:
                 </p>
             </div>
@@ -26,79 +26,141 @@ const Process = () =>{
                 <VerticalTimeline>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                        <p>
-                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Picking Theme + Designing and Planning</h3>
+                        <img src="src/assets/timeline/theme.jpg" alt="theme" style={{width:'100%'}}/>
+                        
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Art Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                        <p>
-                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                        </p>
+                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Mix Designing and Testing</h3>
+                         <img src="src/assets/timeline/mix.png" alt="mix" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                        <p>
-                        User Experience, Visual Design
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Putting Mold together</h3>
+                        <img src="src/assets/timeline/mold.jpg" alt="mold" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                        <p>
-                        User Experience, Visual Design
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>The First Layer of Concrete</h3>
+                        <img src="src/assets/timeline/layerOne.jpeg" alt="layerOne" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-                        <p>
-                        Strategy, Social Media
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Adding Layers of Mesh Reinforcement</h3>
+                        <img src="src/assets/timeline/mesh.png" alt="mesh" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-                        <p>
-                        Creative Direction, User Experience, Visual Design
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Reinforcement in the Gunwale</h3>
+                        <img src="src/assets/timeline/gunwaleMesh.png" alt="gunwale" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
-                        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-                        <p>
-                        Creative Direction, Visual Design
-                        </p>
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>The Second Layer of Concrete</h3>
+                        <img src="src/assets/timeline/layerTwo.png" alt="layerTwo" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
-                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                    />
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Letting the Canoe Set</h3>
+                        <img src="src/assets/timeline/setting.jpg" alt="setting" style={{width:'100%'}}/>
+                        
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Writing the Design Paper</h3>
+                        <img src="src/assets/timeline/designPapers.png" alt="designPapers" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Demolding the Canoe</h3>
+                        <img src="src/assets/timeline/demold.png" alt="demold" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Presentation Preparation</h3>
+                        <img src="src/assets/timeline/presentation.png" alt="presenting" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#629AED', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #629AED' }}
+                        iconStyle={{ background: '#629AED', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Sanding and Finishing the Canoe</h3>
+                        <img src="src/assets/timeline/sanding.jpeg" alt="sanding" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Creating the Sectional</h3>
+                        <img src="src/assets/timeline/sectional.png" alt="sectional" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Constructing the Display</h3>
+                        <img src="src/assets/timeline/display.png" alt="display" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#181c54ff', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #181c54ff' }}
+                        iconStyle={{ background: '#181c54ff', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Competing at the Symposium</h3>
+                        <img src="src/assets/timeline/competition.jpg" alt="competition" style={{width:'100%'}}/>
+                    </VerticalTimelineElement>
                 </VerticalTimeline>
             </div>
             
