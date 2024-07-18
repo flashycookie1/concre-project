3295230c58903c29dfc1697e3d2435fc354043a4
George Ji <flashycookie1@gmail.com>
2024-07-18 18:20:29 -0400

    removed import link which was causing error

--- src/components/navbarFooter.tsx
+++ src/components/navbarFooter.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 const NavbarFooter = () => {
     const uconntact = "https://uconntact.uconn.edu/organization/concretecanoeassociationuconn";
