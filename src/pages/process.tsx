7e0682fb069ea430fcca781ef552e17c4930ec4c
George Ji <flashycookie1@gmail.com>
2024-07-18 21:26:19 -0400

    updated timeline img imports

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -3,6 +3,24 @@ import NavBarFooter from "../components/navbarFooter";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import '../process.css'
 
+import start from '../assets/timeline/start.jpg';
+import end from '../assets/timeline/end.jpeg';
+import theme from '../assets/timeline/theme.jpg'
+import mix from '../assets/timeline/mix.png';
+import mold from '../assets/timeline/mold.jpg';
+import layerOne from '../assets/timeline/layerOne.jpeg';
+import layerTwo from '../assets/timeline/layerTwo.png';
+import mesh from '../assets/timeline/mesh.png';
+import gunwale from '../assets/timeline/gunwaleMesh.png';
+import setting from '../assets/timeline/setting.jpg';
+import designPapers from '../assets/timeline/designPapers.png';
+import demold from '../assets/timeline/demold.png';
+import presenting from '../assets/timeline/presentation.png';
+import sanding from '../assets/timeline/sanding.jpeg';
+import sectional from '../assets/timeline/sectional.png';
+import display from '../assets/timeline/display.png';
+import competition from '../assets/timeline/competition.jpg';
+
 const Process = () =>{
     return(
         <>
@@ -12,11 +30,11 @@ const Process = () =>{
                     How we go from 
                 </p>
                 <div className="small-transitionDiv">
-                    <img src="src/assets/timeline/start.jpg" alt="start" style={{width:'380px'}}/>
+                    <img src={start} alt="start" style={{width:'380px'}}/>
                     <p className="small-trans-text" style={{width:'340px', textAlign:'center'}}>
                         to
                     </p>
-                    <img src="src/assets/timeline/end.jpeg" alt="end" style={{width:'380px'}}/>
+                    <img src={end} alt="end" style={{width:'380px'}}/>
                 </div>
                 <p className="small-trans-text" style={{paddingTop:'50px', textAlign:'center'}}>
                     Check out the Process Below:
@@ -31,7 +49,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Picking Theme + Designing and Planning</h3>
-                        <img src="src/assets/timeline/theme.jpg" alt="theme" style={{width:'100%'}}/>
+                        <img src={theme} alt="theme" style={{width:'100%'}}/>
                         
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
@@ -41,7 +59,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                          <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Mix Designing and Testing</h3>
-                         <img src="src/assets/timeline/mix.png" alt="mix" style={{width:'100%'}}/>
+                         <img src={mix} alt="mix" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -50,7 +68,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Putting Mold together</h3>
-                        <img src="src/assets/timeline/mold.jpg" alt="mold" style={{width:'100%'}}/>
+                        <img src={mold} alt="mold" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -59,7 +77,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>The First Layer of Concrete</h3>
-                        <img src="src/assets/timeline/layerOne.jpeg" alt="layerOne" style={{width:'100%'}}/>                       
+                        <img src={layerOne} alt="layerOne" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -68,7 +86,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Adding Layers of Mesh Reinforcement</h3>
-                        <img src="src/assets/timeline/mesh.png" alt="mesh" style={{width:'100%'}}/>                       
+                        <img src={mesh} alt="mesh" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -77,7 +95,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Reinforcement in the Gunwale</h3>
-                        <img src="src/assets/timeline/gunwaleMesh.png" alt="gunwale" style={{width:'100%'}}/>                       
+                        <img src={gunwale} alt="gunwale" style={{width:'100%'}}/>                       
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -86,7 +104,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>The Second Layer of Concrete</h3>
-                        <img src="src/assets/timeline/layerTwo.png" alt="layerTwo" style={{width:'100%'}}/>
+                        <img src={layerTwo} alt="layerTwo" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -95,7 +113,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Letting the Canoe Set</h3>
-                        <img src="src/assets/timeline/setting.jpg" alt="setting" style={{width:'100%'}}/>
+                        <img src={setting} alt="setting" style={{width:'100%'}}/>
                         
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
@@ -105,7 +123,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Writing the Design Paper</h3>
-                        <img src="src/assets/timeline/designPapers.png" alt="designPapers" style={{width:'100%'}}/>
+                        <img src={designPapers} alt="designPapers" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -114,7 +132,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Demolding the Canoe</h3>
-                        <img src="src/assets/timeline/demold.png" alt="demold" style={{width:'100%'}}/>
+                        <img src={demold} alt="demold" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -123,7 +141,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Presentation Preparation</h3>
-                        <img src="src/assets/timeline/presentation.png" alt="presenting" style={{width:'100%'}}/>
+                        <img src={presenting} alt="presenting" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -132,7 +150,7 @@ const Process = () =>{
                         iconStyle={{ background: '#629AED', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Sanding and Finishing the Canoe</h3>
-                        <img src="src/assets/timeline/sanding.jpeg" alt="sanding" style={{width:'100%'}}/>
+                        <img src={sanding} alt="sanding" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -141,7 +159,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Creating the Sectional</h3>
-                        <img src="src/assets/timeline/sectional.png" alt="sectional" style={{width:'100%'}}/>
+                        <img src={sectional} alt="sectional" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -150,7 +168,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Constructing the Display</h3>
-                        <img src="src/assets/timeline/display.png" alt="display" style={{width:'100%'}}/>
+                        <img src={display} alt="display" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
@@ -159,7 +177,7 @@ const Process = () =>{
                         iconStyle={{ background: '#181c54ff', color: '#fff' }}
                     >
                         <h3 className="vertical-timeline-element-title" style={{textAlign:'center', paddingBottom:'20px'}}>Competing at the Symposium</h3>
-                        <img src="src/assets/timeline/competition.jpg" alt="competition" style={{width:'100%'}}/>
+                        <img src={competition} alt="competition" style={{width:'100%'}}/>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
             </div>
