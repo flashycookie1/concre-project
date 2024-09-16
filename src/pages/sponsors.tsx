81796c83fdf8a8f87b2b19c9dc55988c70433993
Mia Larkin <mia.larkin1@icloud.com>
2024-09-16 17:55:46 -0400

    added the sponor packet to the sponsors page

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,11 +1,20 @@
 import Navbar from "../components/navbar";
 import NavBarFooter from "../components/navbarFooter";
+import spons_pack from "../assets/FINAL_Sponsor_Packet.pdf"
+import '../sponsors.css'
 
 
 const Sponsors = () =>{
     return(
         <>
             <Navbar></Navbar>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent:"center", alignItems: "center",
+                  paddingBottom: "100px" }}>
+                <p className="intro_text">
+                    Check out our Sponsor Packet:
+                </p>
+                <object width="50%"  height="900px" data={spons_pack}  type="application/pdf"/>
+            </div>
             <NavBarFooter></NavBarFooter>
         </>
     )
