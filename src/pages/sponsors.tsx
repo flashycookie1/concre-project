0e2322e90dc8c203bf59005b36838bf9c18793c6
George Ji <gji@sprucetech.com>
2024-10-15 19:37:41 -0400

    added hntb logo to sponsors page.

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,15 +1,26 @@
 import Navbar from "../components/navbar";
 import NavBarFooter from "../components/navbarFooter";
-import spons_pack from "../assets/FINAL_Sponsor_Packet.pdf"
-import '../sponsors.css'
+import spons_pack from "../assets/FINAL_Sponsor_Packet.pdf";
+import '../sponsors.css';
+import HNTB from "../assets/HNTB_Logo.png";
 
 
 const Sponsors = () =>{
     return(
         <>
             <Navbar></Navbar>
-            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignContent:"center", alignItems: "center",
-                  paddingBottom: "100px" }}>
+            <div style={{ display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          alignContent:"center",
+                          alignItems: "center",
+                          paddingBottom: "100px" }}>
+                <p className="intro_text">Thanks to Our Sponsors!</p>
+                <img src={HNTB}
+                    style={{backgroundColor: "white",
+                    padding: 30,
+                    height: 100,
+                }}/>
                 <p className="intro_text">
                     Check out our Sponsor Packet:
                 </p>
